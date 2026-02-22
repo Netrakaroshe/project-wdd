0035d42402322ec39df5a58f0946433dea3b6a7b
Netrakaroshe <netrakaroshe24@gmail.com>
2026-02-22 14:33:15 +0530

    calculator app -> state variables

--- project-app/src/step1/index.jsx
+++ project-app/src/step1/index.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import Addition from "./Addition";
 import Multiplication from "./Multiplication";
 import Division from "./Division";
@@ -6,7 +6,9 @@ import Subtraction from "./subtraction";
 
 
 let Calc = () => {
-    let x=10,y=8;
+    let [x,setX] = useState(0);
+    let [y,setY] = useState(0);
+
 
   return (
     <div>
@@ -14,8 +16,8 @@ let Calc = () => {
                 <div className="border-3 border-blue-900 rounded-xl shadow-2xl m-4 w-1/4">
                     <h1 className="bg-blue-900 text-center text-xl text-white rounded-t-md">Calculator App</h1>
                     <div className="flex">
-                        <input type="text" className="border-1 border-blue-800 p-3 m-2 text-center w-1/2 rounded-md" placeholder="num1" />
-                        <input type="text" className="border-1 border-blue-900 p-3 m-2 text-center w-1/2 rounded-md" placeholder="num2" />
+                        <input onChange = {e=>{setX(Number(e.target.value))}} type="text" className="border-1 border-blue-800 p-3 m-2 text-center w-1/2 rounded-md" placeholder="num1" />
+                        <input onChange = {e=>{setY(Number(e.target.value))}} type="text" className="border-1 border-blue-900 p-3 m-2 text-center w-1/2 rounded-md" placeholder="num2" />
                     </div>
                 </div>
             </div>
