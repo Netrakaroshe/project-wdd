8940c034e8dcc66659d22dee426b31948d4cbdea
Netrakaroshe <netrakaroshe24@gmail.com>
2026-02-22 12:04:52 +0530

    multiplication component included

--- project-app/src/step1/index.jsx
+++ project-app/src/step1/index.jsx
@@ -1,13 +1,15 @@
-import React from 'react'
-import Addition from './Addition';
+import React from "react";
+import Addition from "./Addition";
+import Multiplication from "./Multiplication";
 
 let Calc = () => {
   return (
     <div>
       <p>Calculator App</p>
       <Addition />
+      <Multiplication />
     </div>
-  )
-}
+  );
+};
 
 export default Calc;
