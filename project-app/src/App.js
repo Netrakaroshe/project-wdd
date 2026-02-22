d85b3f9348c7dfc776839325e51d57a2fedfa5d5
Netrakaroshe <netrakaroshe24@gmail.com>
2026-02-22 12:14:22 +0530

    ui changes

--- project-app/src/App.js
+++ project-app/src/App.js
@@ -5,15 +5,7 @@ import Calc from './step1/index';
 function App() {
   return (
     <div className="App">     
-    <div className="flex justify-center">
-                <div className="border-3 border-blue-900 rounded-xl shadow-2xl m-4 w-1/4">
-                    <h1 className="bg-blue-900 text-center text-xl text-white rounded-t-md">Calculator App</h1>
-                    <div className="flex">
-                        <input type="text" className="border-1 border-blue-800 p-3 m-2 text-center w-1/2 rounded-md" placeholder="num1" />
-                        <input type="text" className="border-1 border-blue-900 p-3 m-2 text-center w-1/2 rounded-md" placeholder="num2" />
-                    </div>
-                </div>
-            </div>
+   
      <Calc />
      
     </div>
--- project-app/src/step1/index.jsx
+++ project-app/src/step1/index.jsx
@@ -6,6 +6,15 @@ import Division from "./Division";
 let Calc = () => {
   return (
     <div>
+         <div className="flex justify-center">
+                <div className="border-3 border-blue-900 rounded-xl shadow-2xl m-4 w-1/4">
+                    <h1 className="bg-blue-900 text-center text-xl text-white rounded-t-md">Calculator App</h1>
+                    <div className="flex">
+                        <input type="text" className="border-1 border-blue-800 p-3 m-2 text-center w-1/2 rounded-md" placeholder="num1" />
+                        <input type="text" className="border-1 border-blue-900 p-3 m-2 text-center w-1/2 rounded-md" placeholder="num2" />
+                    </div>
+                </div>
+            </div>
       <p>Calculator App</p>
       <Addition />
       <Multiplication />
